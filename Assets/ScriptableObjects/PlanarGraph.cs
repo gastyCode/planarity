391889d52e6ae63340ba5f4679778e05db0ce8b4
gastyCode <andrejmarkus274@gmail.com>
2022-12-04 20:56:32 +0100

    Graph generation from ScriptableObjects

---
+++ Assets/ScriptableObjects/PlanarGraph.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu]
+public class Graph : ScriptableObject
+{
+    public int level;
+    public Vector2[] positions;
+}
